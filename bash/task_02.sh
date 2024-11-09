94be6a740ad4949e9ad585e75b1db70cf8ded2f0
WoRWn <129893188+WoRWn@users.noreply.github.com>
2024-11-09 16:49:04 +0300

    Delete bash directory

--- bash/task_02.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [[ -z "$VAR1" || -z "$VAR2" ]]; then
-    echo "ERROR"
-else
-    result=$((VAR1+VAR2))
-    echo "$result"
-fi
--- bash/task_07.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-read -r A
-if (( A % 2 == 0 )); then
-    echo "MAMA"
-else
-    echo "PAPA"
-fi
